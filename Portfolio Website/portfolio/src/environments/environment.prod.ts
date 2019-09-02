5d0e6d466e22a83e59f7bc80e8638047b1a6734f
parthjaradi <parthjaradi@gmail.com>
2019-09-02 14:39:27 -0400

    Firebase API Keys secured

--- Portfolio Website/portfolio/src/environments/environment.prod.ts
+++ Portfolio Website/portfolio/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebaseConfig:{
-    apiKey: "AIzaSyD0Vw8vckT_aIAmALnR6nDHpj9jyzXNsHk",
+    apiKey: "*********************************",
     authDomain: "parthjaradiportfolio.firebaseapp.com",
     databaseURL: "https://parthjaradiportfolio.firebaseio.com",
     projectId: "parthjaradiportfolio",
     storageBucket: "",
-    messagingSenderId: "709953219864",
-    appId: "1:709953219864:web:d12f43af7447a442"
+    messagingSenderId: "************",
+    appId: "****************************"
 
   }
 
